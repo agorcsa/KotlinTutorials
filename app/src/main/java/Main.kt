3a3ebacc4e4a9c9ec8908e09df97c9faba963dbc
andreeagorcsa <andreea.gorcsa@t-systems.com>
2021-04-06 16:19:41 +0200

    Encapsulation using: - private keyword - getter method - overriding methods (one method takes String as parameter, the other takes a whole object)

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,48 +1,135 @@
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.N)
 fun main() {
 
-    // loops from 0 to 10
-    for (i in 0..10) {
-        println("$i squared is ${i * i}")
-    }
+    /*println("My first Kotlin line")
+val tim = "Tim"
+println(tim)
 
-    println("====================")
+println()
 
-    // loops from 0 to 9
-    for (i in 0 until 10) {
-        println(i)
-    }
+val weeklySalary = 32
+val timsMonthlySalary : Int = weeklySalary * 4
+println("Tim's weekly salary is $weeklySalary$")
+println("Tim's monthly salary is $timsMonthlySalary$")
 
-    println("====================")
+println()
 
-    // loops from 10 down to 0
-    for (i in 10 downTo 0) {
-        println(i)
-    }
+val apples = 6
+val oranges = 5
+val fruit : Int = apples + oranges
+println("We have $fruit fruits.")
+println("A quarter of the apples is ${apples / 4}")
+println("In the basket we have $apples apples and $oranges oranges, which makes a total of $fruit fruits.")
 
-    println("====================")
+println()
 
-    for (i in 10 downTo 0 step 2) {
-        println(i)
-    }
+val weeks = 234
+val years : Double = weeks/52.0
+println("$weeks weeks in years is $years years")
+println("We have $weeks weeks.")
 
-    println("====================")
+println()
 
-    // challenge
-    // option 1
-    println("The numbers that are both divisible by 3 and 5 are:")
-    for (i in 0..100) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            println(i)
+val jim = "James T. Kirk"
+println("My name is $tim")
+println("Jim is $jim")*/
+
+    // conditionals
+    /*val lives = 3
+
+    var isGameOver = (lives < 1)
+    println(isGameOver)
+
+    if(isGameOver) {
+        println("Game over!")
+    } else {
+        println("You're still alive!")
+    }*/
+
+    /*println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("My age is $age")
+
+    var message = ""
+    message = when {
+        age < 18 -> {
+            "You're too young to vote"
+        }
+        age == 100 -> {
+            "Congratulations!"
+        }
+        else -> {
+            "You can vote"
         }
     }
+    println(message)*/
 
-    println("====================")
+    // classes
+    val tim = Player("Tim")
+    val louise = Player("Louise", 6)
+    val john = Player("John", 4, 8)
+    val mary = Player("Mary", 2, 5, 1000)
 
-    //option 2
-    for (i in 0..100 step 3) {
-        if (i % 5 == 0) {
-            println(i)
-        }
+   // println(mary.weapon.name.toUpperCase())
+   // println(mary.weapon.damageInflicted)
+
+
+    val axe = Weapon("Axe", 12)
+    john.weapon = axe
+    // 2 references to the same object "axe"
+    println(john.weapon.name)
+    println(axe.name)
+
+    axe.damageInflicted = 24
+    println(axe.damageInflicted)
+    println(john.weapon.damageInflicted)
+
+    var sword = Weapon("Sword", 10)
+
+    // direct assignment of the weapon to tim
+    tim.weapon = sword
+    //println(tim.weapon.name)
+
+    // louise takes the weapon from tim
+    louise.weapon = tim.weapon
+    //println(louise.weapon.name)
+
+    tim.weapon = Weapon("Spear", 14)
+    //println(tim.weapon.name)
+
+    tim.show()
+    louise.show()
+    john.show()
+    mary.show()
+
+    val redPotion = Loot("RedPotion", LootType.POTION, 7.50)
+    tim.getLoot(redPotion)
+    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOUR, 80.0)
+    tim.getLoot(chestArmor)
+    tim.getLoot(Loot("+2 Ring of Protection", LootType.RING, 40.25))
+    tim.getLoot(Loot("Invisibility Potion", LootType.POTION, 35.95))
+    tim.showInventory()
+
+    if (tim.dropLoot(redPotion)) {
+        tim.showInventory()
+    } else {
+        println("You don't have a ${redPotion.name}")
+    }
+
+    val bluePotion = Loot("Blue Potion", LootType.POTION, 6.00)
+    if (tim.dropLoot(bluePotion)) {
+        tim.showInventory()
+    } else {
+        println("You don't have a ${bluePotion.name}")
+    }
+
+    if(tim.dropLoot("Invisibility Potion")) {
+        tim.showInventory()
+    } else {
+        println("You don't have an Invisibility Potion")
     }
 }
 
--- app/src/main/java/Player.kt
+++ app/src/main/java/Player.kt
@@ -1,7 +1,11 @@
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 class Player(val name: String, val level: Int = 1, val lives: Int = 3, val score : Int = 0) {
 
     var weapon : Weapon = Weapon("Fist", 1)
-    val inventory = ArrayList<Loot>()
+    // encapsulation
+    private val inventory = ArrayList<Loot>()
 
     fun show() {
         if(lives > 0) {
@@ -19,6 +23,29 @@ class Player(val name: String, val level: Int = 1, val lives: Int = 3, val score
                 score: $score
                 weapon: $weapon
             """
+    }
+
+    // encapsulation
+    fun getLoot(item: Loot) {
+        inventory.add(item)
+    }
+
+    // version 1, parameter Object
+    fun dropLoot(item: Loot) : Boolean {
+        return if (inventory.contains(item)) {
+            inventory.remove(item)
+            true
+        } else {
+            false
+        }
+    }
+
+    // version 2, parameter String
+    // overloading fun dropLoot from above
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun dropLoot(name: String) : Boolean {
+        println("$name will be dropped")
+        return inventory.removeIf { it.name == name }
     }
 
     fun showInventory() {
